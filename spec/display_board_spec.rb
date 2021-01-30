1416304fedb28db8d04e4160ddd58de24b6e9d6c
Constance Mann <connie.mann@yahoo.com>
2021-01-30 19:38:32 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -188,13 +188,4 @@ describe "#display_board in 'lib/display_board.rb" do
   end
 end
 
-def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
-  def display_board(@board)
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-      puts " ----------- "
-      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts " ----------- "
-      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    end
+#
